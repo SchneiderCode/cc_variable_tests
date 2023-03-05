df84223b7e0e5822cb7a74b33ae6e7768e6aaf07
Michael Schneider <schneidm@goldmail.etsu.edu>
2023-03-04 17:56:15 -0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -6,11 +6,11 @@ CircuitCheck.runCircuitCheck(function () {
     CircuitCheck.pause(500)
 })
 let decimal_test = 0
-let bool_test = false
 let text_test = ""
 let int_test = 0
+let bool_test = false
 CircuitCheck.prepareVariables(function () {
-    int_test = CircuitCheck.transmitVariableData("int_test", int_test, Type.Integer)
+    bool_test = CircuitCheck.transmitVariableData("int_test", int_test, Type.Integer)
     text_test = CircuitCheck.transmitVariableData("text_test", text_test, Type.String)
     bool_test = CircuitCheck.transmitVariableData("bool_test", bool_test, Type.Boolean)
     decimal_test = CircuitCheck.transmitVariableData("decimal_test", decimal_test, Type.Decimal)
